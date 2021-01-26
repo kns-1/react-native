3eab26cde0078385bf1e2c6cedc8ebec30781f06
kns-1 <navyakovuru2020@gmail.com>
2021-01-26 13:25:07 +1000

    added icons to draw navigator items

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,9 +4,12 @@ import Home from './HomeComponent';
 import Dishdetail from './DishdetailComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
+import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { Icon } from 'react-native-elements';
+
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -22,7 +25,15 @@ const MenuNavigator = () => {
         headerTitleStyle: {
           color: "#fff",
         },
+      }}
+      
+      options={{
+        headerLeft: () => ( <Icon name="menu" size={24} 
+        color= 'white'
+        onPress={ () => navigation.toggleDrawer() } /> 
+        ),         
       }}>
+
       <Stack.Screen name="Menu" component={Menu} />
       <Stack.Screen name="Dishdetail" component={Dishdetail} />
     </Stack.Navigator>
@@ -40,6 +51,13 @@ const HomeNavigator = () => {
       headerTitleStyle: {
         color: "#fff",
       },
+    }}
+    
+    options={{
+      headerLeft: () => ( <Icon name="menu" size={24} 
+      color= 'white'
+      onPress={ () => navigation.toggleDrawer() } /> 
+      ),         
     }}>
       <Stack.Screen name="Home" component={Home} />
     </Stack.Navigator>
@@ -56,6 +74,13 @@ const AboutNavigator = () => {
       headerTitleStyle: {
         color: "#fff",
       },
+    }}
+    
+    options={{
+      headerLeft: () => ( <Icon name="menu" size={24} 
+      color= 'white'
+      onPress={ () => navigation.toggleDrawer() } /> 
+      ),         
     }}>
       <Stack.Screen name="About Us" component={About} />
     </Stack.Navigator>
@@ -72,6 +97,13 @@ const ContactNavigator = () => {
       headerTitleStyle: {
         color: "#fff",
       },
+    }}
+    
+    options={{
+      headerLeft: () => ( <Icon name="menu" size={24} 
+      color= 'white'
+      onPress={ () => navigation.toggleDrawer() } /> 
+      ),         
     }}>
       <Stack.Screen name="Contact Us" component={Contact} />
     </Stack.Navigator>
@@ -86,20 +118,48 @@ const MainNavigator = () => {
     }}>
     <Drawer.Screen name="Home" component={HomeNavigator} options={{
       title: 'Home',
-      drawerLabel: 'Home'
+      drawerLabel: 'Home',
+      drawerIcon: ({ tintColor }) => (
+        <Icon
+          name='home'
+          type='font-awesome'
+          sise={24}
+          color={tintColor} />
+      )
     }} />
     <Drawer.Screen name="About Us" component={AboutNavigator} options={{
       title: 'About Us',
-      drawerLabel: 'About Us'
+      drawerLabel: 'About Us',
+      drawerIcon: ({ tintColor }) => (
+        <Icon
+          name='info-circle'
+          type='font-awesome'
+          sise={24}
+          color={tintColor} />
+      )
     }} />
     <Drawer.Screen name="Menu" component={MenuNavigator} options={{
       title: 'Menu',
-      drawerLabel: 'Menu'
+      drawerLabel: 'Menu',
+      drawerIcon: ({ tintColor }) => (
+        <Icon
+          name='list'
+          type='font-awesome'
+          sise={24}
+          color={tintColor} />
+      )
     }} />
 
     <Drawer.Screen name="Contact Us" component={ContactNavigator} options={{
       title: 'Contact Us',
-      drawerLabel: 'Contact Us'
+      drawerLabel: 'Contact Us',
+      drawerIcon: ({ tintColor }) => (
+        <Icon
+          name='address-card'
+          type='font-awesome'
+          sise={22}
+          color={tintColor} />
+      )
     }} />
     </Drawer.Navigator>
   );
